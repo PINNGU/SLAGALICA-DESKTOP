e5dda1d70aefe57672ec445d7bc14e54a4b9a21f
Cvijetinovic Nikola <dzonic25@gmail.com>
2023-08-06 18:49:27 +0200

    can open and print json

--- LUCKY_6/main.py
+++ LUCKY_6/main.py
@@ -15,20 +15,22 @@ c = 0
 stake = 0
 win = 0
 
-f = open("stats.txt","w")
-g = open("ticket.txt","w")
+f = open("stats.txt","w") #stats
+g = open("ticket.txt","w") #ticket
+fjs = open("signs.json","w") # json
 while(not game.run_alot()):     # this will run until lucky loser,gold or silver bonuses - can be changed in the class method
     
     c = c + 1
     stake = stake + money
     win = win + game.get_full_game_wins()
 
-game.get_stats(f) #this will print only the last iteration,or the one that got it out of the loop
+game.get_stats(f,fjs) #this will print only the last iteration,or the one that got it out of the loop
                   #for printint all iterations,include the function in the loop
 game.get_ticket(g)  #a real life representation of the gambling ticket
 
 g.close()
 f.close()
+fjs.close()
 print(f"Total bid amount: {stake} , Won amount: {win} , Profit: {win - stake}\n")
 print(f"Total games: {c}")
 
