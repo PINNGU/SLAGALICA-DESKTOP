4a61fec9d84e7ffae224d3406080b7f7f19b5d6f
Cvijetinovic Nikola <dzonic25@gmail.com>
2023-08-16 22:27:52 +0200

    starting gametwo,git bugged

--- SLAGALICA_DESKTOP/gametwo.py
+++ SLAGALICA_DESKTOP/gametwo.py
@@ -1,7 +1,18 @@
 import numpy as np
 import PySimpleGUI as psg
+import random
 
 class GameTwo():
 
     def __init__(self,theme):
         self.theme = theme
+
+    def get_digits(self):
+
+        digits = random.choices(range(1,10),k = 4)
+        mid_dig = random.choice((5,10,20))
+        hight_dig = random.choice((25,50,75,100))
+
+        digits.extend((mid_dig,hight_dig))
+        return digits
+    
