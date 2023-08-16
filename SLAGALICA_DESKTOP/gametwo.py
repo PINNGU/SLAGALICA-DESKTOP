008031c9bed468a6c2e14c388dd45cf44af9fc11
Cvijetinovic Nikola <dzonic25@gmail.com>
2023-08-17 01:02:16 +0200

    finished logic,window missing

--- SLAGALICA_DESKTOP/gametwo.py
+++ SLAGALICA_DESKTOP/gametwo.py
@@ -9,10 +9,84 @@ class GameTwo():
 
     def get_digits(self):
 
-        digits = random.choices(range(1,10),k = 4)
-        mid_dig = random.choice((5,10,20))
+        digits = random.sample(range(1,10),k = 4)
+        mid_dig = random.choice((5,10,15,20))
         hight_dig = random.choice((25,50,75,100))
 
         digits.extend((mid_dig,hight_dig))
         return digits
     
+    def is_divisible(self,one,two,three = 0):
+        ret = True
+
+        if one % two != 0:
+            ret = False
+        if three != 0:
+            if one % (two + three) != 0:
+                ret = False
+        
+            if (one  + two) % three != 0:
+                ret = False
+
+        return ret    
+    def get_signs(self):
+        ex = []
+        digits = self.get_digits()
+        i = 5
+        while True:
+
+            if self.is_divisible(digits[i],digits[i - 1],digits[i - 2]):
+                sign = random.choice(["/","/","/","/","*","+","-","("])
+            elif i == 0:
+                sign = random.choice(["*","+","-"])
+            else:
+                sign = random.choice(("*","+","-","("))
+
+            if sign == "(":
+                
+                ex.append(sign)
+                ex.append(str(digits[i]))
+                
+
+                if self.is_divisible(digits[i],digits[i - 1]):
+                    sign = random.choice(["/","/","/","*","+","-"])
+                  
+                else:
+                    sign = random.choice(("*","+","-"))
+
+                i = i -1
+                ex.append(sign)
+                ex.append(str(digits[i]))
+                i = i - 1
+                ex.append(")")
+                
+            else:
+                ex.append(str(digits[i]))
+                i = i - 1
+            if i < 0:
+                break
+
+            if self.is_divisible(digits[i],digits[i - 1],digits[i - 2]):
+                sign = random.choice(["/","/","/","*","+","-"])
+            else:
+                sign = random.choice(("*","+","-"))
+            ex.append(sign)
+                
+        ex = "".join(ex)
+        res = round(eval(ex))
+        return res,ex,digits
+    
+
+    def get_number(self):
+        while True:
+            res,ex,digits = self.get_signs()
+
+            if  99 < res < 999:
+                self.number = int(res)
+                self.digits = np.array(digits)
+                self.solution = ex
+                print(f"{ex}  =  {res} , {digits}")
+                break
+    
+    def make_window(self):
+        pass # implement
--- SLAGALICA_DESKTOP/main.py
+++ SLAGALICA_DESKTOP/main.py
@@ -4,26 +4,26 @@ from gametwo import GameTwo
 import json
 
 GAME_THEME = "DarkBlue10"
-VERSION = "V1.2"
+VERSION = "V1.3"
 
 player = Login(GAME_THEME,VERSION)
-finished_login = player.start_win()#
+#finished_login = player.start_win()#
 #if finished_login:
-file = open("words.json","r")
+#file = open("words.json","r")
 
-words = json.load(file)
-words = tuple(words.keys())
-game_one = GameOne(GAME_THEME,words)
+#words = json.load(file)
+#words = tuple(words.keys())
+#game_one = GameOne(GAME_THEME,words)
 
-game_one_finished = game_one.start_win()
-player.score = player.score + game_one.won_points # get the won points in first game,conc
+#game_one_finished = game_one.start_win()
+#player.score = player.score + game_one.won_points # get the won points in first game,conc
 
-if game_one_finished:  # if the first game has concluded and the player chose to keep playing
-    game_two = GameTwo(GAME_THEME)  # init the second game
+#if game_one_finished:  # if the first game has concluded and the player chose to keep playing
+game_two = GameTwo(GAME_THEME)  # init the second game
 
+game_two.get_number()
 
-
-file.close()
+#file.close()
 
 
 
